3dbab482db32dfa7f0d808d883616d669ed54a20
Vitya Endless <dvitya031190@gmail.com>
2024-06-02 16:34:28 +0600

    added product and review models

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Product, Review
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at']
+    list_display_links = ['id', 'title']
+    list_filter = ['title', 'created_at', 'category']
+    search_fields = ['title', 'description', 'category']
+    fields = ['title', 'category', 'description', 'img', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'text', 'created_at']
+    list_display_links = ['id', 'text']
+    list_filter = ['created_at', 'product', 'author', 'status', 'grade']
+    search_fields = ['text', 'status', 'product', 'author', 'grade']
+    fields = ['text', 'grade', 'status', 'product', 'author', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class IntegerRangeField(models.IntegerField):
+    def __init__(self, min_value=None, max_value=None, **kwargs):
+        self.validators = [MinValueValidator(min_value), MaxValueValidator(max_value)]
+        super().__init__(**kwargs)
+
+
+categories = [('1', 'Good'), ('2', 'Service')]
+moderate_statuses = [('1', 'Moderate'), ('2', 'Approve'), ('3', 'Rejection')]
+
+
+class AbstractModel(models.Model):
+    created_at = models.DateTimeField(verbose_name="Created at", auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated at")
+
+    class Meta:
+        abstract = True
+
+
+class Product(AbstractModel):
+    title = models.CharField(max_length=20, null=False, blank=False, verbose_name="Title")
+    category = models.CharField(max_length=30, null=False, blank=False, verbose_name='Category', choices=categories)
+    description = models.TextField(max_length=500, verbose_name="Description")
+    img = models.ImageField(upload_to='publications_images', verbose_name='Avatar')
+
+    def __str__(self):
+        return self.title
+
+
+class Review(AbstractModel):
+    product = models.ForeignKey(
+        'webapp.Product', related_name='reviews', on_delete=models.CASCADE, verbose_name='Product'
+    )
+    author = models.ForeignKey(
+        get_user_model(), default=1, related_name='reviews', on_delete=models.SET_DEFAULT, verbose_name='Author'
+    )
+    text = models.TextField(max_length=400, null=False, blank=False, verbose_name='Comment')
+    grade = IntegerRangeField(min_value=1, max_value=5, null=False, blank=False, verbose_name='Grade')
+    status = models.CharField(max_length=30, default=1, verbose_name='Status', choices=moderate_statuses)
+
+    def __str__(self):
+        return f'{self.pk} review'
