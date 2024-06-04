90377a075b18dd670487a0988ffc0869aec53ebd
Vitya Endless <dvitya031190@gmail.com>
2024-06-04 22:39:59 +0600

    added reviews list view

--- source/webapp/views/products_views.py
+++ source/webapp/views/products_views.py
@@ -56,6 +56,11 @@ class ProductView(DetailView):
     model = Product
     template_name = 'products/product_details.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['reviews'] = self.object.reviews.all()
+        return context
+
 
 class ProductUpdateView(UpdateView):
     template_name = 'products/product_update.html'
